0162780df5cc22b09146fdb67d365de4cd4ac5a4
Kautism <darkautism@gmail.com>
2022-10-09 01:33:04 +0800

    加入專案檔案。

---
+++ RiseOfErosLocalization/Plugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+using HarmonyLib;
+using Pinkcore.Heros.Localization;
+using UnityEngine;
+
+namespace RiseOfErosLocalization
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class RiseOfErosLocalization : BasePlugin
+    {
+        public override void Load()
+        {
+            Harmony.CreateAndPatchAll(typeof(RiseOfErosLocalization));
+            Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(LocalizationManager), "Pinkcore_Heros_Localization_ILocalizationManager_GetText")]
+        public static void GetTextPatch(string term, ref LocalizationLanguage language, LocalizationManager __instance) {
+            if (__instance._currentLanguage != LocalizationLanguage.ChineseTraditional)
+                __instance._currentLanguage = LocalizationLanguage.ChineseTraditional;
+        }
+    }
+}
---
+++ RiseOfErosLocalization/Readme.md
@@ -0,0 +1,5 @@
+﻿# 欲神幻想DMM PC版中文化
+
+1. Install [BepInEx_UnityIL2CPP_x64_6.0.0-pre.1.zip](https://github.com/BepInEx/BepInEx/releases/download/v6.0.0-pre.1/BepInEx_UnityIL2CPP_x64_6.0.0-pre.1.zip)
+2. Run Game Once. This steup will hang a minute. If you watching the Game UI, you will found BepInEx\plugins dir is generate. Then close the game.
+3. Extract dll into BepInEx\plugins. 
